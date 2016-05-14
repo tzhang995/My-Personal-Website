cc8beab9fb36b085477a7107da747cd87b7c050b
Tony Zhang <tzhang995@gmail.com>
2016-05-14 17:57:30 -0400

    What is this css thing?

--- education.php
+++ education.php
@@ -10,9 +10,25 @@
 		<?php include("header.php");?>
 
 		<!--Body paragraph-->
-		<div id="inner" class="container">
-			<div class="body-paragraph">
-
+		<div id="inner">
+			<div class="myList">
+				<div class="myListItem container-fluid">
+					<div class="row">
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								University of Waterloo
+							</div>
+							<div class="myListTime">
+								January 2016 - May 2016
+							</div>
+							<div class="myListSummary">
+							</div>
+						</div>
+						<div class="myListLogo col-sm-3 col-md-3 col-lg-3">
+							<img src="img/Pivotal_TealOnWhite.png">
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 
