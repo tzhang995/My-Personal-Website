f3aadcef45fdc806b342d434744eb19571fa4472
Tony Zhang <tzhang995@gmail.com>
2016-05-16 13:29:27 -0400

    added more courses

--- education.php
+++ education.php
@@ -42,6 +42,55 @@
 								<p>CS 245 — Logic and Computation</p>
 								<p>STAT 231 — Statistics</p>
 								<p>MATH 136 — Linear Algebra 1 for Honours Mathematics</p>
+								<p>SPCOM 228 — Public Communications</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="myListItem container-fluid">
+					<div class="row">
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								September - December 2015, 2A
+							</div>
+							<div class="myListSummary">
+								<p>CS 246 — Object-Oriented Software Development</p>
+								<p>CS 251 — Computer Organization and Design</p>
+								<p>STAT 230 — Probability</p>
+								<p>MATH 135 — Algebra for Honours Mathematics</p>
+								<p>STV 100 — Society, Technology and Values</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="myListItem container-fluid">
+					<div class="row">
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								January - April 2015, 1B
+							</div>
+							<div class="myListSummary">
+								<p>CS 136 — Elementary Algorithm Design and Data Abstraction</p>
+								<p>MATH 128 — Calculus 2 for the Sciences</p>
+								<p>GEOG 281 — Introduction to Geographic Information Systems (GIS)</p>
+								<p>GEOG 102 — Geography and Our Planetary Environment</p>
+								<p>ENVS 178 — Introduction to Environmental Research Methods</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="myListItem container-fluid">
+					<div class="row">
+						<div class="myListText myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								September - December 2014, 1A
+							</div>
+							<div class="myListSummary">
+								<p>CS 135 — Designing Functional Programs</p>
+								<p>MATH 127 — Calculus 1 for the Sciences</p>
+								<p>GEOG 187 — Problem Solving in Geomatics</p>
+								<p>GEOG 101 — Geography and Human Habitat</p>
+								<p>ENGL 109 — Introduction to Academic Writing</p>
 							</div>
 						</div>
 					</div>
