30b4b5b9ecdd458a6fd8754ab0c3f8d9382b3e95
Tony Zhang <tzhang995@gmail.com>
2016-08-29 18:47:34 -0400

    adding N Things page

--- blog.php
+++ blog.php
@@ -14,6 +14,11 @@
 				This page is more or less just updating myself, create and access my side projects and self learning. Honestly, it is to learn JavaScript and the many, many, many (sigh) libraries that it has. Feel free to roam around.
 			</p>
 		</div>
+		 <div class="list-group">
+		  <a href="#" class="list-group-item">Algorithms</a>
+		  <a href="./nThings.php" class="list-group-item">N Things</a>
+		  <a href="#" class="list-group-item">Other Projects</a>
+		</div>
 	</body>
 	<?php include("script.php");?>
 </html>
---
+++ nThings.php
@@ -0,0 +1,22 @@
+<!--NEED PHP HEADER-->
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include("head.php"); ?>
+	</head>
+
+	<body>
+		<?php include("header.php");?>
+		<!--Body paragraph-->
+		<div class="container-fluid">
+			<div class="container-fluid main-head">
+				<div class="body-title">
+					N Things
+				</div>
+			</div>
+
+			<
+
+		</div>
+	</body>
+</html>
