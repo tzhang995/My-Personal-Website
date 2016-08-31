5f1506ef1d3595f6dfb17be981af8e3d297447be
Tony Zhang <tzhang995@gmail.com>
2016-08-31 15:32:02 -0400

    updated lists

--- nThings.php
+++ nThings.php
@@ -13,10 +13,33 @@
 				<div class="body-title">
 					N Things
 				</div>
+
+				<div class="body-desc">
+					<p>This page will be will with lists about random things. I don't know why I'm doing this. Mainly because I saw it from MathSoc and thought that idea was very cool. I'm gonna try to make my own.</p>
+				</div>
+			</div>
+		
+			<div class="container-fluid list-container">
+				<div class="list-head">
+					<p>N Things of N Lists</p>
+				</div>
+				<div class="list-desc">
+					<p>Lol this will just be a list of lists that I want to in the future. Mostly cause I tend to forget so might as well write it down.</p>
+				</div>
+				<div class="list">
+					<ul>
+						<li>N Companies I would LOVE to work for</li>
+						<li>N Things I would like to do in life</li>
+						<li>N Programming languages that I like</li>
+						<li>N Programming languages that I dislike</li>
+						<li>N Quality Mangas</li>
+						<li>N Best karaoke songs</li>
+						<li>N Countries I would live to visit</li>
+					</ul>
+				</div>
 			</div>
 
-			<
-
 		</div>
+
 	</body>
 </html>
