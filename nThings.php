d320e06695c14ce0937a1e442941199490702149
Tony Zhang <tzhang995@gmail.com>
2016-09-05 22:33:57 -0400

    some css

--- nThings.php
+++ nThings.php
@@ -19,24 +19,29 @@
 				</div>
 			</div>
 		
-			<div class="container-fluid list-container">
-				<div class="list-head">
-					<p>N Things of N Lists</p>
-				</div>
-				<div class="list-desc">
-					<p>Lol this will just be a list of lists that I want to in the future mostly cause I tend to forget so might as well write it down.</p>
-				</div>
-				<div class="list-group">
-					<a href="#" class="list-group-item">N Reasons I became a programmer</a>
-					<a href="#" class="list-group-item">N Companies I would LOVE to work for</a>
-					<a href="#" class="list-group-item">N Things I would like to do in life</a>
-					<a href="#" class="list-group-item">N Programming languages that I like</a>
-					<a href="#" class="list-group-item">N Quality Mangas</a>
-					<a href="#" class="list-group-item">N Best karaoke songs</a>
-					<a href="#" class="list-group-item">N Countries I would live to visit</a>
+			<div class="myList">
+				<div class="myListItem container-fluid">
+					<div class="row">
+						<div class="myListText col-sm-9 col-md-9 col-lg-9">
+							<div class="myListTitle">
+								<p>N Things of N Lists</p>
+							</div>
+							<div class="myListSummary">
+								<p>Lol this will just be a list of lists that I want to in the future mostly cause I tend to forget so might as well write it down.</p>
+							</div>
+							<div class="list-group">
+								<a href="#" class="list-group-item">N Reasons I became a programmer</a>
+								<a href="#" class="list-group-item">N Companies I would LOVE to work for</a>
+								<a href="#" class="list-group-item">N Things I would like to do in life</a>
+								<a href="#" class="list-group-item">N Programming languages that I like</a>
+								<a href="#" class="list-group-item">N Quality Mangas</a>
+								<a href="#" class="list-group-item">N Best karaoke songs</a>
+								<a href="#" class="list-group-item">N Countries I would live to visit</a>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-
 		</div>
 
 	</body>
