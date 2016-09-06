2caa54512e1b53d1d9e3eafea623243220709e3b
Tony Zhang <tzhang995@gmail.com>
2016-09-05 22:38:44 -0400

    some css

--- nThings.php
+++ nThings.php
@@ -30,13 +30,15 @@
 								<p>Lol this will just be a list of lists that I want to in the future mostly cause I tend to forget so might as well write it down.</p>
 							</div>
 							<div class="myListList">
-								<a href="#" class="myListListItem">N Reasons I became a programmer</a>
-								<a href="#" class="myListListItem">N Companies I would LOVE to work for</a>
-								<a href="#" class="myListListItem">N Things I would like to do in life</a>
-								<a href="#" class="myListListItem">N Programming languages that I like</a>
-								<a href="#" class="myListListItem">N Quality Mangas</a>
-								<a href="#" class="myListListItem">N Best karaoke songs</a>
-								<a href="#" class="myListListItem">N Countries I would live to visit</a>
+								<ul>
+									<li><a href="#" class="myListListItem">N Reasons I became a programmer</a></li>
+									<li><a href="#" class="myListListItem">N Companies I would LOVE to work for</a></li>
+									<li><a href="#" class="myListListItem">N Things I would like to do in life</a></li>
+									<li><a href="#" class="myListListItem">N Programming languages that I like</a></li>
+									<li><a href="#" class="myListListItem">N Quality Mangas</a></li>
+									<li><a href="#" class="myListListItem">N Best karaoke songs</a></li>
+									<li><a href="#" class="myListListItem">N Countries I would live to visit</a></li>
+								</ul>
 							</div>
 						</div>
 					</div>
